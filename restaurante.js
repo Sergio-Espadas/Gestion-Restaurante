bdf5f7a154e2562638fb4f7f76b944bf8927751c
Sergio <sergesp1416@gmail.com>
2024-01-06 18:14:02 +0100

    Objeto dish creado

---
+++ restaurante.js
@@ -0,0 +1,8 @@
+class Dish {
+    constructor(name, description = "", ingredients = [], image = "") {
+        this.name = name;
+        this.description = description;
+        this.ingredients = ingredients;
+        this.image = image;
+    }
+}

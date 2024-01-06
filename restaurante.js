c105ab5f4a9f5a62ee1fd81feda50afbf04b57ea
Sergio <sergesp1416@gmail.com>
2024-01-06 18:26:26 +0100

    Objeto category creado

--- restaurante.js
+++ restaurante.js
@@ -1,8 +1,19 @@
-class Dish {
+
+// Objeto para identifica los datos de un plato
+class dish {
     constructor(name, description = "", ingredients = [], image = "") {
         this.name = name;
         this.description = description;
         this.ingredients = ingredients;
         this.image = image;
+    }
+}
+
+
+// Con este objeto podemos crear la estructura de categorías
+class category {
+    constructor(name, description = "") {
+        this.name = name;
+        this.description = description;
     }
 }
